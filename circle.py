a4886432c38751588b7d6e8cc982798225257edf
Vlad Isaev <vladisaev600@mail.ru>
2024-10-18 00:09:27 +0300

    Documentation

--- circle.py
+++ circle.py
@@ -1,10 +1,29 @@
 import math
 
-
 def area(r):
+    ''' 
+    Вычисляет площадь оркужности радиуса r
+    по формуле:
+        area = pi * r^2
+
+        Входные данные:
+            double r : радиус окружности
+
+        Выходные данные:
+            double area : площадь оружности
+    '''
     return math.pi * r * r
 
-
 def perimeter(r):
-    return 2 * math.pi * r
+    '''
+    Вычисляет длину окружности 
+    радиуса r по формуле:
+        perimeter = 2 * pi * r
 
+        Входные данные:
+            double r : радиус окружности
+        
+        Выходные данные:
+            double perimeter : длина окружности радиуса r
+    '''
+    return 2 * math.pi * r
